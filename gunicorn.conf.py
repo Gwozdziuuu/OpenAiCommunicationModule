e169bdf602541074d16824a82ba0464d19932a71
Marcin Gwóźdź <marcin.gwozdz@testto.com>
2025-09-12 10:00:08 +0200

    fix: log configuration

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -7,7 +7,7 @@ import os
 from src.config import Config
 
 # Serwer
-bind = f"{Config.HOST}:{Config.PORT}"
+bind = f"{Config.HOST()}:{Config.PORT()}"
 workers = int(os.getenv('GUNICORN_WORKERS', '4'))
 worker_class = "sync"
 worker_connections = int(os.getenv('GUNICORN_WORKER_CONNECTIONS', '1000'))
@@ -15,7 +15,7 @@ timeout = int(os.getenv('GUNICORN_TIMEOUT', '30'))
 keepalive = int(os.getenv('GUNICORN_KEEPALIVE', '5'))
 
 # Logowanie
-loglevel = Config.LOG_LEVEL.lower()
+loglevel = Config.LOG_LEVEL().lower()
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
 accesslog = '-'  # stdout
 errorlog = '-'   # stderr
